8aa18ee7562bee448ce9b752cc116a5af8df46d6
keer1912 <S10194171@connect.np.edu.sg>
2021-11-02 12:18:49 +0800

    Add home activity layout

---
+++ app/src/main/java/sg/edu/np/pfd_ocbc/HomeActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.np.pfd_ocbc;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import sg.edu.np.pfd_ocbc.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
