4f8ba3433e7ad80bc1e273149e6ccf9cbb30d094
keer1912 <S10194171@connect.np.edu.sg>
2021-11-02 12:18:57 +0800

    Add home activity layout

---
+++ app/src/main/java/sg/edu/np/pfd_ocbc/LoginActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.np.pfd_ocbc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
