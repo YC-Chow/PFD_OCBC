b85df7bba44de8ec1b8239087af6593cdd5df054
Yun Cong <S10194206@connect.np.edu.sg>
2021-11-02 12:41:13 +0800

    Add Account, Card, Transaction classes

---
+++ app/src/main/java/sg/edu/np/pfd_ocbc/Transaction.java
@@ -0,0 +1,64 @@
+package sg.edu.np.pfd_ocbc;
+
+import java.util.Date;
+
+public class Transaction {
+    private String toPhoneNum;
+    private String toIC;
+    private String toBankNum;
+    private Date transactionDate;
+    private String transactionId;
+
+    public String getToPhoneNum() {
+        return toPhoneNum;
+    }
+
+    public String getToIC() {
+        return toIC;
+    }
+
+    public String getToBankNum() {
+        return toBankNum;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setToPhoneNum(String toPhoneNum) {
+        this.toPhoneNum = toPhoneNum;
+    }
+
+    public void setToIC(String toIC) {
+        this.toIC = toIC;
+    }
+
+    public void setToBankNum(String toBankNum) {
+        this.toBankNum = toBankNum;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Transaction() {
+    }
+
+    public Transaction(String toPhoneNum, Date transactionDate, String transactionId) {
+        this.toPhoneNum = toPhoneNum;
+        this.transactionDate = transactionDate;
+        this.transactionId = transactionId;
+    }
+
+
+
+
+}
