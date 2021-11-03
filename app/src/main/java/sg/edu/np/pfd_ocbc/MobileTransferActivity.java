ac5d89dcc3339f0de6338da591a7f6ec3848e05a
Yun Cong <S10194206@connect.np.edu.sg>
2021-11-03 14:11:57 +0800

    Update MobileTransferActivity.java

--- app/src/main/java/sg/edu/np/pfd_ocbc/MobileTransferActivity.java
+++ app/src/main/java/sg/edu/np/pfd_ocbc/MobileTransferActivity.java
@@ -1,5 +1,6 @@
 package sg.edu.np.pfd_ocbc;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,10 +21,19 @@ import androidx.navigation.ui.NavigationUI;
 public class MobileTransferActivity extends AppCompatActivity {
 
     ImageView transferBtn;
+    TextView customNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_transfer);
+        customNum.findViewById(R.id.customNumber);
 
+        customNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MobileTransferActivity.this, MobileNumberActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
