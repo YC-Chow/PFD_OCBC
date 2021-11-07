d483772f1612176f68647c8e0294d55e9cabe932
JinYana <ngjinyang@gmail.com>
2021-11-08 01:16:48 +0800

    Login validation done, display user information in profile and setting done

--- app/src/main/java/sg/edu/np/pfd_ocbc/ProfileActivity.java
+++ app/src/main/java/sg/edu/np/pfd_ocbc/ProfileActivity.java
@@ -3,22 +3,43 @@ package sg.edu.np.pfd_ocbc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
 
         //Setting up bottom nav bar
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -68,5 +89,30 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        TextView name = findViewById(R.id.username);
+        TextView phone = findViewById(R.id.mobileno);
+        TextView email = findViewById(R.id.profileemail);
+
+        SharedPreferences sharedPref = getSharedPreferences("Profile", MODE_PRIVATE);
+
+        String username = sharedPref.getString("Name", "");
+        String userphone = sharedPref.getString("Phone", "");
+        String useremail = sharedPref.getString("Email", "");
+
+        name.setText(username);
+        phone.setText(userphone);
+        email.setText(useremail);
+        Log.v("hi", useremail);
+
+
+
+
+
+
+
     }
+
+
+
 }
