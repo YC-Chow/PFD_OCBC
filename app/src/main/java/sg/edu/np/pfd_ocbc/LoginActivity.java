58ea539f495d88de421679e06521f7d074c0aa3c
keer1912 <S10194171@connect.np.edu.sg>
2021-11-17 16:09:55 +0800

    Update UI

--- app/src/main/java/sg/edu/np/pfd_ocbc/LoginActivity.java
+++ app/src/main/java/sg/edu/np/pfd_ocbc/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
+
 import android.os.Build;
 import android.os.Bundle;
 
@@ -19,6 +20,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -260,7 +262,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button signup = findViewById(R.id.signup);
+        TextView signup = findViewById(R.id.signup);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
