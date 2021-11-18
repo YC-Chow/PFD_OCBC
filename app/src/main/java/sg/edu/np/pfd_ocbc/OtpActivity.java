6b5808103d6114f8d9d3953503fedc3df979384d
Chow Yun Cong <S10194206@connect.np.edu.sg>
2021-11-18 16:22:19 +0800

    Add back button listener

--- app/src/main/java/sg/edu/np/pfd_ocbc/OtpActivity.java
+++ app/src/main/java/sg/edu/np/pfd_ocbc/OtpActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -59,6 +60,8 @@ public class OtpActivity extends AppCompatActivity {
         EditText opt = findViewById(R.id.otp);
         TextView resend = findViewById(R.id.resend);
         Button submit = findViewById(R.id.submit);
+        ImageView back = findViewById(R.id.backBtnOTP);
+
         paragraph.setText("A one-time password has been sent to your phone ("+phoneNo+").");
         requestOTP(phoneNo);
 //        createAccountHolder();
@@ -74,6 +77,15 @@ public class OtpActivity extends AppCompatActivity {
                 if (!opt.getText().toString().equals("")){
                     submitOTP(phoneNo,opt.getText().toString(),situation);
                 }
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(OtpActivity.this, LoginActivity.class);
+                startActivity(intent1);
+                finish();
             }
         });
     }
